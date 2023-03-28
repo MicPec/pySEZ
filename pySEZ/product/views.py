a1cb5ef9709ed610b8e8c7d9a1ff93df9946e0a8
MicPec <michal.pecyna@gmail.com>
2023-03-28 21:03:00 +0200

    screenshots and readme

--- pySEZ/product/views.py
+++ pySEZ/product/views.py
@@ -63,8 +63,8 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["title"] = "Nowy Produkt"
-        context["action"] = reverse("product_create")
+        context["title"] = "New product"
+        context["action"] = reverse("product-create")
         return context
 
 
---
+++ readme.md
@@ -0,0 +1,9 @@
+Simple order record system.
+
+**Work in progress**
+
+![screenshot 1](img/scr1.png)
+![screenshot 2](img/scr2.png)
+![screenshot 3](img/scr3.png)
+![screenshot 4](img/scr4.png)
+
